1b2a6aebb39bcb57c862dbfaf4bffbefb956b9c4
Clark Johnson <clark@stontech.net>
2020-05-28 14:14:44 +0000

    Working to solve in one pass

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
+// Leetcode Rotate Array
+//
 var rotate = function(nums, k) {
   let temp;
   let offset = 0 
   let ll = nums.length
   if (k > 0 ){
     for (let i=0;i < ll-1;i++){
+      if (i >= (parseInt(ll/k)*k)) {
+        k= k-(ll%k)
+      }
       let temp = nums[i]
       j = nums.length - k + offset
       nums[i] = nums[j]
       nums[j]=temp 
-      console.log(nums,offset)
+      console.log(nums,i,j,k)
       offset = offset ===  k-1 ? 0: offset+1
     }
   }
    return nums 
 };
 
-console.log(rotate([1,2,3,4,5,6,7],5))
+console.log(rotate([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16],10))
 // console.log(9)
