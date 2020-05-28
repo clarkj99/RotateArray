663d44594035578a7453b12cb1073cfae10d559b
Clark Johnson <clark@stontech.net>
2020-05-28 17:22:38 +0000

    Used reverse function to rotate the array

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 // Leetcode Rotate Array
-//
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/646/
+
 var rotate = function(nums, k) {
-  let temp;
-  let offset = 0 
+  k = k % nums.length
   let ll = nums.length
-  if (k > 0 ){
-    for (let i=0;i < ll-1;i++){
-      if (i >= (parseInt(ll/k)*k)) {
-        k= k-(ll%k)
-      }
-      let temp = nums[i]
-      j = nums.length - k + offset
-      nums[i] = nums[j]
-      nums[j]=temp 
-      console.log(nums,i,j,k)
-      offset = offset ===  k-1 ? 0: offset+1
+  var reverseArray = function(start, end) {
+    for (i = start; i < end - i + start; i++) {
+      let temp = nums[i];
+      nums[i] = nums[end - i + start];
+      nums[end - i + start]= temp;
     }
-  }
-   return nums 
+  } 
+
+  reverseArray(0,nums.length-1)
+
+reverseArray(0, k-1);
+reverseArray(k,nums.length-1);
+return nums 
 };
 
-console.log(rotate([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16],10))
+console.log(rotate([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],16))
 // console.log(9)
